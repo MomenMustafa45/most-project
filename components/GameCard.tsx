02519fdc26940cf10654df53775fe2dfcf998d1c
MomenMustafa45 <momenkara2@gmail.com>
2024-07-10 17:58:40 +0300

    fix: style of game card fixed

--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -15,7 +15,7 @@ const GameCard = ({ price, title }: CardProps) => {
   return (
     <View className="w-[85%] mx-auto bg-[#C514B0] p-2 rounded-2xl my-2 shadow-lg">
       {/* <Image source={image} className="w-full rounded-md" /> */}
-      <View className="w-full h-[100px] bg-blue-600 rounded-xl flex items-center just">
+      <View className="w-full h-[100px] bg-blue-600 rounded-xl flex items-center justify-center">
         <BeakerIcon size={50} color={"yellow"} />
       </View>
       {/*  */}
