cda0752e3272fae747ed8feb0c624c5858d8891a
MomenMustafa45 <momenkara2@gmail.com>
2024-07-11 09:16:04 +0300

    feat: reward screen added

--- app/Rewards.tsx
+++ app/Rewards.tsx
@@ -3,8 +3,10 @@ import React from "react";
 
 const Rewards = () => {
   return (
-    <View>
-      <Text>Rewards</Text>
+    <View className="flex-1 bg-[#4E08D7] ">
+      <Text className="text-white text-center py-5 text-lg font-bold">
+        Select your reward
+      </Text>
     </View>
   );
 };
