115527c154513bd8616ea374898397e102130263
MomenMustafa45 <momenkara2@gmail.com>
2024-07-11 09:15:31 +0300

    feat: myGames screen added

--- app/MyGames.tsx
+++ app/MyGames.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
+import SectionTitle from "@/components/SectionTitle";
 
 const MyGames = () => {
   return (
-    <View>
-      <Text>MyGames</Text>
+    <View className="flex-1 bg-[#4E08D7]">
+      <View className=" my-6">
+        <SectionTitle title="MY GAMES" />
+      </View>
     </View>
   );
 };
